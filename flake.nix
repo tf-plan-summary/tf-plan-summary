d9419ae1ed1f910e824e3047f7ba9a892ead4dbf
Loïs Postula <lois@postu.la>
2025-07-01 15:21:10 +0200

    chore: bootstrap goreleaser

--- flake.nix
+++ flake.nix
@@ -46,6 +46,9 @@
                 packages = with pkgs; [
                   updatecli
                   goreleaser
+                  golangci-lint
+                  cosign
+                  syft
                   treefmtEval.config.build.wrapper
                 ];
                 languages = {
@@ -67,12 +70,14 @@
                   # golang
                   gofmt.enable = true;
                   golangci-lint.enable = true;
+                  revive.enable = false;
                 };
                 enterTest = ''
-                  go mod verify
-                  goreleaser check
-                  go test -coverprofile=cover.out $(go list ./... | grep -v /cmd | grep -v /claims | grep -v /team)
-                  coverage=$(go tool cover -func=cover.out | grep total | awk '{print substr($3, 1, length($3)-1)}')
+                  ${pkgs.go}/bin/go mod verify
+                  ${pkgs.golangci-lint}/bin/golangci-lint run
+                  ${pkgs.goreleaser}/bin/goreleaser check
+                  ${pkgs.go}/bin/go test -coverprofile=cover.out $(go list ./... | grep -v /cmd | grep -v /claims | grep -v /team)
+                  coverage=$(${pkgs.go}/bin/go tool cover -func=cover.out | grep total | awk '{print substr($3, 1, length($3)-1)}')
                   if (( $(echo "$coverage < 25" | bc -l) )); then
                     echo "Test coverage is below 25s%: $coverage%"
                     exit 1
@@ -91,15 +96,15 @@
                   };
                   "build.all" = {
                     exec = ''
-                      ${pkgs.goreleaser}/bin/goreleaser build --clean --skip=publish,sign
+                      ${pkgs.goreleaser}/bin/goreleaser build --clean --timeout 2h
                     '';
-                    description = "Snapshot build";
+                    description = "Release build";
                   };
                   lint = {
                     exec = ''
                       ${pkgs.golangci-lint}/bin/golangci-lint run
                     '';
-                    description = "Snapshot build";
+                    description = "Linting";
                   };
                 };
 
